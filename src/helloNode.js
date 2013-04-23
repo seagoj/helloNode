3a2c05d9e42b7a0406a57ed4e590ef7f8d8bf8da
Jeremy Seago <seagoj@gmail.com>
2013-04-23 14:08:42 -0500

    Adds child process execution to Server

--- src/helloNode.js
+++ src/helloNode.js
@@ -3,7 +3,23 @@ var url = '127.0.0.1';
 var port = 1337;
 
 http.createServer(function (req,res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello Node!\n');
+    var util = require('util'),
+        spawn = require('child_process').spawn,
+        ls = spawn('ls', ['-lh', '/usr']);
+
+    ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data );
+    });
+
+    ls.on('exit', function (code){
+        console.log('child process exited with code ' + code);
+    });
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Node!\n');
 }).listen(port, url);
 console.log('Server running at http://'+url+':'+port+'/');
